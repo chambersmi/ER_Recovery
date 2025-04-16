6f3cb0290bf9aa59168194f0ebc1c8bb50ed6e7c
Mike Chambers <mjchambers83@live.com>
2025-04-15 21:41:58 -0400

    Added Resources Razor Page

---
+++ ER_Recovery.Web/Pages/AA/Resources.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ER_Recovery.Web.Pages.AA
+{
+    public class ResourcesModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
