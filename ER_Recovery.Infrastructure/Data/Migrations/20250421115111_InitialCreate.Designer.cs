60ce8acb7f3f44726ba10179cccca4336c2567df
Mike Chambers <mjchambers83@live.com>
2025-04-21 07:52:30 -0400

    Created DB

--- ER_Recovery.Infrastructure/Data/Migrations/20250421115111_InitialCreate.Designer.cs
+++ ER_Recovery.Infrastructure/Data/Migrations/20250421115111_InitialCreate.Designer.cs
@@ -11,7 +11,7 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace ER_Recovery.Infrastructure.Data.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    [Migration("20250419003400_InitialCreate")]
+    [Migration("20250421115111_InitialCreate")]
     partial class InitialCreate
     {
         /// <inheritdoc />
